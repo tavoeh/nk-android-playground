b95b191d40dbf9cc6d5f5951d1cffe259f86de32
Gustavo Enriquez <gustavo.enriquez@nike.com>
2022-02-04 14:27:53 -0800

    Adding sample RV adapter.

--- app/src/main/java/com/tavoeh/playground/presentation/DummyListAdapter.kt
+++ app/src/main/java/com/tavoeh/playground/presentation/DummyListAdapter.kt
@@ -1,4 +1,52 @@
 package com.tavoeh.playground.presentation
 
-class DummyListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+/** Usage
+val adapter = DummyListAdapter {
+Toast.makeText(context, "Item Clicked $it", Toast.LENGTH_SHORT).show()
 }
+adapter.submitList(sampleItemList)
+ */
+
+class DummyListAdapter(
+    private val onItemClick: (Item) -> Unit
+) : ListAdapter<Item, ItemViewHolder>(ItemDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemBinding = parent.inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ItemViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = currentList[position]
+        with(holder.view) {
+            findViewById<TextView>(android.R.id.text1).text = item.text
+            setOnClickListener { onItemClick(item) }
+        }
+    }
+}
+
+class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+object ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item) = oldItem.id == newItem.id
+    override fun areContentsTheSame(oldItem: Item, newItem: Item) = oldItem == newItem
+}
+
+data class Item(val id: String, val text: String)
+
+val sampleItemList = listOf(
+    Item("001", "Text 1"),
+    Item("002", "Text 2"),
+    Item("003", "Text 3")
+)
+
+val View.inflater: LayoutInflater
+    get() = LayoutInflater.from(this.context)
